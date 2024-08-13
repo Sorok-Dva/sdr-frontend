1d82574ccdd4d7c3895ec7f53cb8c5c1dfe982ac
Сорок два <sorokdva.developer@gmail.com>
2024-08-13 19:53:56 +0200

    ✨ chore: comment out unused components in Home.tsx
    Comment out unused components to streamline code. This improves render
    performance and reduces unnecessary imports for cleaner maintenance and
    readability going forward.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,31 @@
 import React from 'react'
 import MainBanner from '../components/HomeOne/MainBanner'
-import Features from '../components/HomeOne/Features'
-import About from '../components/HomeOne/About'
 import Services from '../components/HomeOne/Services'
 import Newsletter from '../components/Common/Newsletter'
-import Testimonials from '../components/Common/Testimonials'
-import WhatWeOffer from '../components/HomeOne/WhatWeOffer'
 import News from '../components/Common/News'
-import Partner from '../components/Common/Partner'
 const Home : React.FC = () => {
   
   return (
     <>
       <MainBanner />
       
-      <Features />
+      {/*<Features />*/}
       
-      <About />
+      {/* <About />*/}
       
       <Services />
       
       <Newsletter />
       
-      <Testimonials />
+      {/*<Testimonials />*/}
       
-      <WhatWeOffer />
+      {/*<WhatWeOffer />*/}
       
       <News />
       
-      <div className="pb-50">
+      {/*      <div className="pb-50">
         <Partner />
-      </div>
+      </div>*/}
     </>
   )
 }
