d1137f1c6f3096687c4f11e9fbcaf99df241139a
Сорок два <sorokdva.developer@gmail.com>
2024-08-14 17:03:14 +0200

    🖼️ update: Revamp ReportList UI and data structure
    Revise ReportList UI to use PageBanner. Replace screenshotId and screenshotUrl with dreamId, and remove screenshotUrl filtering.

--- src/pages/admin/ReportList.tsx
+++ src/pages/admin/ReportList.tsx
@@ -14,12 +14,12 @@ import {
 import { useAuth } from 'context/AuthContext'
 import { Link } from 'react-router-dom'
 import { FaEye } from 'react-icons/fa6'
+import PageBanner from 'components/Common/PageBanner'
 
 interface AggregatedReport {
-  screenshotId: number;
+  dreamId: number;
   earliestReportDate: Date;
   reportCount: number;
-  screenshotUrl: string;
   reportedBy: string;
   reportedById: number;
 }
@@ -68,8 +68,7 @@ const ReportsList: React.FC = () => {
   const filteredReports = reports.filter((report) =>
     report.reportedBy.toLowerCase().includes(searchTerm.toLowerCase()) ||
     report.reportCount.toString().includes(searchTerm) ||
-    new Date(report.earliestReportDate).toLocaleDateString().includes(searchTerm) ||
-    report.screenshotUrl.toLowerCase().includes(searchTerm.toLowerCase())
+    new Date(report.earliestReportDate).toLocaleDateString().includes(searchTerm)
   )
   
   const sortedReports = [...filteredReports].sort((a, b) => {
@@ -94,122 +93,122 @@ const ReportsList: React.FC = () => {
   
   return (
     <>
-      <Container fluid className="pt-8">
-        <Row className="mb-3">
-          <Col>
-            <h2>Reports List</h2>
-            <p>Total Reports: {filteredReports.length}</p>
-          </Col>
-          <Col>
-            <Input
-              type="text"
-              placeholder="Search in all fields"
-              value={searchTerm}
-              onChange={handleSearch}
-            />
-          </Col>
-        </Row>
-        <Table hover>
-          <thead>
-            <tr>
-              <th onClick={() => handleSort('screenshotId')}>
-              ID {sortedField === 'screenshotId' && (isAsc ? '↑' : '↓')}
-              </th>
-              <th onClick={() => handleSort('screenshotUrl')}>
-              URL {sortedField === 'screenshotUrl' && (isAsc ? '↑' : '↓')}
-              </th>
-              <th onClick={() => handleSort('reportedBy')}>
-              Screen author {sortedField === 'reportedBy' && (isAsc ? '↑' : '↓')}
-              </th>
-              <th onClick={() => handleSort('reportCount')}>
-              Report Count {sortedField === 'reportCount' && (isAsc ? '↑' : '↓')}
-              </th>
-              <th onClick={() => handleSort('earliestReportDate')}>
-              Earliest Report Date {sortedField === 'earliestReportDate' && (isAsc ? '↑' : '↓')}
-              </th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentReports.length === 0 ? (
+      <PageBanner
+        pageTitle="Reports list"
+        homePageUrl="/admin"
+        homePageText="Admin"
+        activePageText="[Admin] Reports list"
+      />
+      <div className="pt-100 pb-70">
+        <div className="container">
+          <Row className="mb-3">
+            <Col>
+              <h2>Reports List</h2>
+              <p>Total Reports: {filteredReports.length}</p>
+            </Col>
+            <Col>
+              <Input
+                type="text"
+                placeholder="Search in all fields"
+                value={searchTerm}
+                onChange={handleSearch}
+              />
+            </Col>
+          </Row>
+          <Table hover>
+            <thead>
               <tr>
-                <td colSpan={6} className="text-center">
-                No active reports found
-                </td>
+                <th onClick={() => handleSort('dreamId')}>
+              ID {sortedField === 'dreamId' && (isAsc ? '↑' : '↓')}
+                </th>
+                <th onClick={() => handleSort('reportedBy')}>
+               Author {sortedField === 'reportedBy' && (isAsc ? '↑' : '↓')}
+                </th>
+                <th onClick={() => handleSort('reportCount')}>
+              Report Count {sortedField === 'reportCount' && (isAsc ? '↑' : '↓')}
+                </th>
+                <th onClick={() => handleSort('earliestReportDate')}>
+              Earliest Report Date {sortedField === 'earliestReportDate' && (isAsc ? '↑' : '↓')}
+                </th>
+                <th>Actions</th>
               </tr>
-            ) : (
-              currentReports.map((report) => (
-                <tr key={report.screenshotId}>
-                  <td>{report.screenshotId}</td>
-                  <td>
-                    <Link to={`/${report.screenshotUrl}`} target="_blank">
-                      {report.screenshotUrl}
-                    </Link>
-                  </td>
-                  <td>
-                    <Link to={`/admin/users/${report.reportedById}`}>
-                      {report.reportedBy}
-                    </Link>
-                  </td>
-                  <td>
-                    <Badge pill color="primary">
-                      {report.reportCount}
-                    </Badge>
-                  </td>
-                  <td>{new Date(report.earliestReportDate).toLocaleDateString()}</td>
-                  <td className="d-flex justify-content-between">
-                    <Link to={`/admin/reports/${report.screenshotId}/details`}>
-                      <Button className="btn-icon btn-2 mb-3" color="success" type="button">
-                        <span className="btn-inner--icon">
-                          <FaEye />
-                        </span>
-                      </Button>
-                    </Link>
+            </thead>
+            <tbody>
+              {currentReports.length === 0 ? (
+                <tr>
+                  <td colSpan={6} className="text-center">
+                No active reports found
                   </td>
                 </tr>
-              ))
-            )}
-          </tbody>
-        </Table>
-        <Pagination>
-          <PaginationItem disabled={currentPage === 1}>
-            <PaginationLink first onClick={() => paginate(1)} />
-          </PaginationItem>
-          <PaginationItem disabled={currentPage === 1}>
-            <PaginationLink previous onClick={() => paginate(currentPage - 1)} />
-          </PaginationItem>
-          {[...Array(totalPages)].map((_, index) => (
-            <PaginationItem active={index + 1 === currentPage} key={index}>
-              <PaginationLink onClick={() => paginate(index + 1)}>
-                {index + 1}
-              </PaginationLink>
+              ) : (
+                currentReports.map((report) => (
+                  <tr key={report.dreamId}>
+                    <td>{report.dreamId}</td>
+                    <td>
+                      <Link to={`/admin/users/${report.reportedById}`}>
+                        {report.reportedBy}
+                      </Link>
+                    </td>
+                    <td>
+                      <Badge pill color="primary">
+                        {report.reportCount}
+                      </Badge>
+                    </td>
+                    <td>{new Date(report.earliestReportDate).toLocaleDateString()}</td>
+                    <td className="d-flex justify-content-between">
+                      <Link to={`/admin/reports/${report.dreamId}/details`}>
+                        <Button className="btn-icon btn-2 mb-3" color="success" type="button">
+                          <span className="btn-inner--icon">
+                            <FaEye />
+                          </span>
+                        </Button>
+                      </Link>
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </Table>
+          <Pagination>
+            <PaginationItem disabled={currentPage === 1}>
+              <PaginationLink first onClick={() => paginate(1)} />
             </PaginationItem>
-          ))}
-          <PaginationItem disabled={currentPage === totalPages}>
-            <PaginationLink next onClick={() => paginate(currentPage + 1)} />
-          </PaginationItem>
-          <PaginationItem disabled={currentPage === totalPages}>
-            <PaginationLink last onClick={() => paginate(totalPages)} />
-          </PaginationItem>
-          <Col>
-            <Input
-              type="select"
-              value={rowsPerPage}
-              onChange={(e) => {
-                setRowsPerPage(Number(e.target.value))
-                setCurrentPage(1) // Reset to first page when changing rows per page
-              }}
-              className="w-auto"
-            >
-              {[10, 30, 50, 100, 500].map((size) => (
-                <option key={size} value={size}>
-                  {size} rows per page
-                </option>
-              ))}
-            </Input>
-          </Col>
-        </Pagination>
-      </Container>
+            <PaginationItem disabled={currentPage === 1}>
+              <PaginationLink previous onClick={() => paginate(currentPage - 1)} />
+            </PaginationItem>
+            {[...Array(totalPages)].map((_, index) => (
+              <PaginationItem active={index + 1 === currentPage} key={index}>
+                <PaginationLink onClick={() => paginate(index + 1)}>
+                  {index + 1}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+            <PaginationItem disabled={currentPage === totalPages}>
+              <PaginationLink next onClick={() => paginate(currentPage + 1)} />
+            </PaginationItem>
+            <PaginationItem disabled={currentPage === totalPages}>
+              <PaginationLink last onClick={() => paginate(totalPages)} />
+            </PaginationItem>
+            <Col>
+              <Input
+                type="select"
+                value={rowsPerPage}
+                onChange={(e) => {
+                  setRowsPerPage(Number(e.target.value))
+                  setCurrentPage(1) // Reset to first page when changing rows per page
+                }}
+                className="w-auto"
+              >
+                {[10, 30, 50, 100, 500].map((size) => (
+                  <option key={size} value={size}>
+                    {size} rows per page
+                  </option>
+                ))}
+              </Input>
+            </Col>
+          </Pagination>
+        </div>
+      </div>
     </>
   )
 }
