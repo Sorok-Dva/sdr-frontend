1ebd0ab294f2671dfc1b8ce7bb8666cca11a4c34
Сорок два <sorokdva.developer@gmail.com>
2024-08-15 11:14:13 +0200

    :recycle: refactor: Simplify error handling for ServiceUnavailable
    Consolidate ServiceUnavailable page by using Error503 component. Updated
    the 503 error component to include PageBanner and align content with
    site's visual style.

--- src/components/ErrorPage/503.tsx
+++ src/components/ErrorPage/503.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { Button, Col, Row } from 'reactstrap'
 import { FaDiscord, FaRotateRight } from 'react-icons/fa6'
+import PageBanner from 'components/Common/PageBanner'
 
 const CustomErrorContent: React.FC = () => {
   const handleReload = () => {
@@ -10,37 +11,38 @@ const CustomErrorContent: React.FC = () => {
   }
   return (
     <>
-      <div className="error-area">
-        <div className="d-table">
-          <div className="d-table-cell">
-            <div className="error-content-wrap">
-              <h1>
-                5 <span>0</span> 3
-              </h1>
-              <Row>
-                <Col>
-                  <img
-                    src="https://i.pinimg.com/originals/7e/1a/1e/7e1a1e67b467b09d48ae7d2dd40fca1a.gif"
-                    alt="Service Unavailable GIF"
-                    className="img-fluid mb-4"
-                    style={ { maxWidth: '100%', height: 'auto', margin: '0 auto' } }
-                  />
-                  <h1>Service Temporarily Unavailable</h1>
-                  <p>
-                    We&apos;re sorry, but the service is currently unavailable. Please try again later.
-                  </p>
-                  <p>
-                    <i>You can join our discord server to get notified when service is back.</i>
-                  </p>
-                  <Button color="success" onClick={ handleReload } className="col-md-3">
-                    <FaRotateRight/> Reload Page
-                  </Button>
-                  <Button color="primary" href="https://discord.gg/adHWrnzcqd" className="col-md-3">
-                    <FaDiscord/> Join discord server
-                  </Button>
-                </Col>
-              </Row>
-            </div>
+      <PageBanner
+        pageTitle="Erreur Serveur"
+        homePageUrl="/"
+        homePageText="Accueil"
+        activePageText="Serveur indisponible"
+      />
+      <div className="pt-50 pb-70">
+        <div className="container">
+          <div className="error-area">
+            <Row>
+              <Col>
+                <img
+                  src="https://i.pinimg.com/originals/7e/1a/1e/7e1a1e67b467b09d48ae7d2dd40fca1a.gif"
+                  alt="Service Unavailable GIF"
+                  className="img-fluid mb-4"
+                  style={ { maxWidth: '100%', height: 'auto', margin: '0 auto' } }
+                />
+                <h1>Service Temporairement Indisponible</h1>
+                <p>
+                  Nous sommes désolé, mais le serveur est actuellement indisponible. Veuillez réessayer plus tard.
+                </p>
+                <p>
+                  <i>Vous pouvez aussi rejoindre notre serveur discord pour être notifié lorsque le serveur sera de nouveau disponible</i>
+                </p>
+                <Button color="success" onClick={ handleReload } className="col-md-3">
+                  <FaRotateRight/> Actualiser
+                </Button>
+                <Button color="primary" href="https://discord.gg/sdr" className="col-md-3">
+                  <FaDiscord/> Rejoindre le serveur discord
+                </Button>
+              </Col>
+            </Row>
           </div>
         </div>
       </div>
--- src/pages/ServiceUnavailable.tsx
+++ src/pages/ServiceUnavailable.tsx
@@ -1,65 +1,9 @@
 import React from 'react'
-import { Button, Col, Container, Row } from 'reactstrap'
-import { FaDiscord, FaRotateRight } from 'react-icons/fa6'
+import Error503 from '../components/ErrorPage/503'
 
 const ServiceUnavailable : React.FC = () => {
-  const handleReload = () => {
-    window.location.href = '/'
-  }
- 
   return (
-    <section className="section section-shaped section-lg">
-      <div className="shape shape-style-1 bg-gradient-default">
-        <span/>
-        <span/>
-        <span/>
-        <span/>
-        <span/>
-        <span/>
-        <span/>
-        <span/>
-      </div>
-      <Container className="text-center mt-2 pt-5">
-        <Row>
-          <Col>
-            <img
-              src="https://i.pinimg.com/originals/7e/1a/1e/7e1a1e67b467b09d48ae7d2dd40fca1a.gif"
-              alt="Service Unavailable GIF"
-              className="img-fluid mb-4"
-              style={ { maxWidth: '100%', height: 'auto', margin: '0 auto' } }
-            />
-            <h1>Service Temporarily Unavailable</h1>
-            <p>
-              We&apos;re sorry, but the service is currently unavailable. Please try again later.
-            </p>
-            <p>
-              <i>You can join our discord server to get notified when service is back.</i>
-            </p>
-            <Button color="success" onClick={ handleReload } className="col-md-3">
-              <FaRotateRight/> Reload Page
-            </Button>
-            <Button color="primary" href="https://discord.gg/adHWrnzcqd" className="col-md-3">
-              <FaDiscord/> Join discord server
-            </Button>
-          </Col>
-        </Row>
-      </Container>
-      <div className="separator separator-bottom separator-skew zindex-100">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          preserveAspectRatio="none"
-          version="1.1"
-          viewBox="0 0 2560 100"
-          x="0"
-          y="0"
-        >
-          <polygon
-            className="fill-white"
-            points="2560 0 2560 100 0 100"
-          />
-        </svg>
-      </div>
-    </section>
+    <Error503 />
   )
 }
 
