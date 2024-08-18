97799330c80cb249446b580143560e1e42a3a357
Сорок два <sorokdva.developer@gmail.com>
2024-08-18 21:03:52 +0200

    :recycle: refactor: Replace News with LastTutorials component
    Simplify code by renaming the imported component to LastTutorials.
    Remove commented-out sections for better code clarity and readability.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,30 +2,20 @@ import React from 'react'
 import MainBanner from '../components/LandingPage/MainBanner'
 import Services from '../components/LandingPage/Services'
 // import Newsletter from '../components/Common/Newsletter'
-import News from 'components/Common/LastTutorials'
+import LastTutorials from 'components/Common/LastTutorials'
 const LandingPage : React.FC = () => {
-  
+
   return (
     <>
       <MainBanner />
-      
-      {/*<Features />*/}
-      
-      {/* <About />*/}
-      
+
       <Services />
-      
+
       {/*<Newsletter />*/}
-      
+
       {/*<Testimonials />*/}
-      
-      {/*<WhatWeOffer />*/}
-      
-      <News />
-      
-      {/*      <div className="pb-50">
-        <Partner />
-      </div>*/}
+
+      <LastTutorials />
     </>
   )
 }
