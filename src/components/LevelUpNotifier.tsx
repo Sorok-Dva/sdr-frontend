2f4b354dbbf83a711021ee7ac7480c698de3ef3f
Сорок два <sorokdva.developer@gmail.com>
2024-08-24 15:42:26 +0200

    🔧 refactor: Remove unused useEffect import
    Removed the unnecessary `useEffect` import from `LevelUpNotifier.tsx`. This cleanup helps streamline the component and slightly improves performance by eliminating unused code.

--- src/components/LevelUpNotifier.tsx
+++ src/components/LevelUpNotifier.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useUser } from 'context/UserContext'
 import useLevelUpNotifications from 'hooks/levelUpNotif'
 
 const LevelUpNotifier: React.FC = () => {
   const { user } = useUser()
 
-  // Appelez le hook inconditionnellement
   useLevelUpNotifications({ userId: user ? user.id : -1 })
 
-  return null // Pas besoin de retour visuel pour ce composant
+  return null
 }
 
 export default LevelUpNotifier
